f29a751dcd2ac8b7028a578109cd1c405a826d66
ludvik bergstrom <ludvikbergstrom@gmail.com>
2025-11-20 13:58:52 +0100

    door mechanic displays ui
    doors now display players required to progress to next level

--- PlatformerPeak/Assets/NextLevel.cs
+++ PlatformerPeak/Assets/NextLevel.cs
@@ -1,14 +1,46 @@
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class NextLevel : MonoBehaviour
 {
     public string nextSceneName;
+    private TextMeshProUGUI playerDoorText;
+    private int playersInDoor = 0;
+
+    private void Start()
+    {
+        playerDoorText = GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+    private void Update()
+    {
+        if (playersInDoor > 0) {
+        playerDoorText.text = playersInDoor + "/2";
+        }
+        else
+        {
+            playerDoorText.text = "";
+        }
+
+        if (playersInDoor == 2)
+        {
+            SceneController.instance.LoadScene(nextSceneName);
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-           SceneController.instance.LoadScene(nextSceneName);
+            playersInDoor += 1;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playersInDoor -= 1;
         }
     }
 }
