83e8f70fe41ba81b4d1f120603444953ea7fcb9c
Amsand <amund.sandersen@gmail.com>
2025-11-21 10:54:42 +0100

    Transition

--- PlatformerPeak/Assets/NextLevel.cs
+++ PlatformerPeak/Assets/NextLevel.cs
@@ -3,12 +3,14 @@ using UnityEngine;
 
 public class NextLevel : MonoBehaviour
 {
-    public string nextSceneName;
+
+
+   
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-           SceneController.instance.LoadScene(nextSceneName);
+            SceneController.instance.NextLevel();
         }
     }
 }
--- PlatformerPeak/Assets/SceneController.cs
+++ PlatformerPeak/Assets/SceneController.cs
@@ -1,9 +1,11 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
 {
     public static SceneController instance;
+    [SerializeField] Animator transitionAnim;
 
     private void Awake()
     {
@@ -14,14 +16,26 @@ public class SceneController : MonoBehaviour
         }
         else
         {
-            Destroy(gameObject);
+            var player = GameObject.FindWithTag("Player");
+            if (player != null)
+                Destroy(player);
         }
     }
 
-    public void LoadScene(string sceneName)
+    public void NextLevel()
     {
-        SceneManager.LoadSceneAsync(sceneName);
+        StartCoroutine(LoadLevel());
+    }
+    IEnumerator LoadLevel()
+    {
+        transitionAnim.SetTrigger("End");
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+        transitionAnim.SetTrigger("Start");
+
     }
 
-
 }
+
+
+
